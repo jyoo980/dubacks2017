ee8fd6cf54dd9e4307dfea34f6822e95e12e46a3
amyjzhu <ajyzhu@gmail.com>
2017-10-28 18:52:38 -0700

    Fix double-sending issue

--- src/main/response/Listener.ts
+++ src/main/response/Listener.ts
@@ -92,32 +92,6 @@ export default class Listener {
         });
     }
 
-    sendResponse(psid : string, response : any) {
-            // Construct the message body
-            let request_body = {
-                "recipient": {
-                    "id": psid
-                },
-                "message": response
-            };
-
-            console.log(request_body);
-
-            // Send the HTTP request to the Messenger Platform
-            request({
-                "uri": "https://graph.facebook.com/v2.6/me/messages",
-                "qs": { "access_token": PAGE_ACCESS_TOKEN },
-                "method": "POST",
-                "json": request_body
-            }, (err : any, res : any, body : any) => {
-                if (!err) {
-                    console.log('message sent!');
-                } else {
-                    console.error("Unable to send message:" + err);
-                }
-            });
-        }
-
         processNormalMessage(res : any, body : any) {
             body.entry.forEach((entry: any) => {
 
@@ -150,8 +124,6 @@ export default class Listener {
                 //  console.log(webhookEvent);
             });
 
-            // Returns '200 OK' response to all requests
-            res.send('EVENT_RECEIVED\n');
 }
 
 
