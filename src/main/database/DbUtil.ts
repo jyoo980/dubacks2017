965a7aade2a0a7a04334b034027f02a521dd6a43
amyjzhu <ajyzhu@gmail.com>
2017-11-30 19:02:28 -0800

    Refactor connection slightly, add update function

--- src/main/database/DbUtil.ts
+++ src/main/database/DbUtil.ts
@@ -6,6 +6,13 @@ export class DbUtil {
 
 // possible to only pass in a function that consumes a connection?
 
+    private static executeSql = function(sql : string, connection : any) {
+        connection.query(sql, function (err: any, result: any) {
+            if (err) throw err;
+            console.log("Yay, no error! Database insert was fine.");
+        })
+    };
+
     // yeah idk if this will work, haha.
     public static doWithConnection(execute : (connection : any) => any) {
         let connection : any = mysql.createConnection(
@@ -22,18 +29,19 @@ export class DbUtil {
 
     // unsafe atm!!
     // abstract further even
+    // should probably also keep a user id in the profile...
     public static insertPerson(person : User, comments : string) {
         let sql : string = " INSERT INTO person " +
             " (name, seller, registration, comments) " +
             ' VALUES ("' + person.getName() + '", 1, NOW(), "' + comments + '");';
-        let executeSql = function(connection : any) {
-            connection.query(sql, function (err: any, result: any) {
-                if (err) throw err;
-                console.log("Yay, no error! Database insert was fine.");
-            })
-        };
-        DbUtil.doWithConnection(executeSql);
+
+        DbUtil.doWithConnection((connection : any) => this.executeSql(sql, connection));
         console.log("Done doWithConnection");
     }
 
+    public static updateProfile(person : User, field : string, newValue : string) {
+        let sql : string = "UPDATE person SET " + field + " = " + newValue
+        + " WHERE person.name = "+ person.getName() + " ;"; // !!! change to unique key!
+        DbUtil.doWithConnection((connection : any) => this.executeSql(sql, connection));
+    }
 }
